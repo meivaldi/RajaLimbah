dbd1ce43679b7db1ee2082b1e080e1c408f4cf25
meivaldi <meivaldi@gmail.com>
2020-10-19 16:44:08 +0700

    Done

--- app/src/main/java/com/meivaldi/rajalimbah/RegisterActivity.java
+++ app/src/main/java/com/meivaldi/rajalimbah/RegisterActivity.java
@@ -2,8 +2,14 @@ package com.meivaldi.rajalimbah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,6 +24,13 @@ import com.meivaldi.rajalimbah.api.ApiInterface;
 import com.meivaldi.rajalimbah.model.ApiResponse;
 import com.meivaldi.rajalimbah.model.User;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,6 +41,11 @@ public class RegisterActivity extends AppCompatActivity {
             companyET, companyPhoneET, companyFaxET, kbliET, companyTypeET;
     private TextView fileName;
     private Button download, upload, register;
+
+    private ProgressDialog pDialog;
+    private static final int progress_bar_type = 0;
+
+    private static String file_url = "https://kmgemilang.com/limbah/android/formulir.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +71,13 @@ public class RegisterActivity extends AppCompatActivity {
         download = findViewById(R.id.download);
         upload = findViewById(R.id.upload);
         register = findViewById(R.id.register);
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DownloadFileFromURL().execute(file_url);
+            }
+        });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,6 +150,69 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    protected Dialog onCreateDialog(int id){
+        switch (id){
+            case progress_bar_type:
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Sedang Men-download, Silahkan Tunggu!");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+                return pDialog;
+            default:
+                return null;
+        }
+    }
+
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+        protected  void onPreExecute(){
+            super.onPreExecute();
+            showDialog(progress_bar_type);
+        }
+
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                int lenghOfFile = connection.getContentLength();
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/formulir.pdf");
+                byte data[] = new byte[1024];
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    publishProgress(""+(int)((total*100)/lenghOfFile));
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+                output.close();
+                input.close();
+            } catch (Exception e){
+                Log.e("Error : ", e.getMessage());
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        protected void onPostExecute(String file_url){
+            dismissDialog(progress_bar_type);
+
+            Toast.makeText(getApplicationContext(), "Download Berhasil, " + Environment.getExternalStorageDirectory().toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
